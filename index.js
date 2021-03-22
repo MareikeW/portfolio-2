eb6ce8818d3c110fb42bd840a70bb06b4741ae3a
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-03-22 18:36:00 +0100

    Projektseite Wetter-App

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const heroSection = document.getElementById("hero-section");
 const heroIntro = document.getElementById("hero-intro");
 const heroMenu = document.getElementById("hero-menu");
+const clickPrompt = document.getElementById("click");
 
 heroSection.addEventListener("click", function change() {
     if (heroIntro.style.display === "none") {
         
     } else {
         heroIntro.style.display = "none"; 
-        heroMenu.style.display = "block";
+        heroMenu.classList.add('transitioned');
+        clickPrompt.style.display = "none";
     }
 })
 
