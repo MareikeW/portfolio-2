d791a4b9dcb23a78675654c703187864ea02ce04
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-03-25 11:55:46 +0100

    Hero-Intro Übergang erstellt

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const heroSection = document.getElementById("hero-section");
 const heroIntro = document.getElementById("hero-intro");
 const heroMenu = document.getElementById("hero-menu");
 const clickPrompt = document.getElementById("click");
+const heroImage = document.getElementById("hero-img");
+/* gibt die aktuelle Browserbreite aus */
+let intViewportWidth = window.innerWidth;
 
 heroSection.addEventListener("click", function change() {
     if (heroIntro.style.display === "none") {
         
+    } else if (intViewportWidth >= 600 && intViewportWidth < 800) {
+        heroImage.style.marginLeft = "0";
+        heroMenu.style.marginRight = "40px";
+        addTransition();
+    }
+    
+    else if (intViewportWidth >= 800){
+        heroImage.style.marginLeft = "0";
+        heroMenu.style.marginRight = "150px";
+        addTransition();
+        
     } else {
-        heroIntro.style.display = "none"; 
-        heroMenu.classList.add('transitioned');
-        clickPrompt.style.display = "none";
+        addTransition();
     }
 })
 
+function addTransition() {
+    heroIntro.style.display = "none"; 
+    heroMenu.classList.add('transitioned');
+    clickPrompt.style.display = "none";
+    heroImage.style.marginTop = "0";
+}
 
